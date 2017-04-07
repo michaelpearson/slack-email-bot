09e5c1dae4ac61f66e9f2e239d09c789e33a9e60
Michael Pearson <zl2mjp@gmail.com>
2017-04-07 14:44:18 +1200

    Add guts

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,92 @@
-import static spark.Spark.get;
+import com.overzealous.remark.Remark;
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import spark.Request;
+import spark.Response;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import java.io.*;
+import java.net.*;
+
 import static spark.Spark.port;
+import static spark.Spark.post;
 
 public class Main {
+
     public static void main(String[] args) {
         port(Integer.valueOf(System.getenv("PORT")));
-        get("/", (req, res) -> "Hello World");
+        Main main = new Main();
+        post("/incoming/*", main::handleRequest);
+    }
+
+    private String handleRequest(Request request, Response response) throws ParseException {
+        String[] segments = request.uri().split("/");
+        String channelName = segments[segments.length - 1];
+        String webHookUrl = System.getenv("#" + channelName);
+        if(webHookUrl == null) {
+            response.status(404);
+            return "Channel not found";
+        }
+        JSONObject incomingEmail = (JSONObject) new JSONParser().parse(request.body());
+
+        try {
+            URL endpoint = new URL(webHookUrl);
+            HttpURLConnection connection = (HttpURLConnection)endpoint.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setDoOutput(true);
+            connection.getOutputStream().write(buildMessage(incomingEmail).toJSONString().getBytes());
+            connection.getInputStream().close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.status(500);
+            return "Failed";
+        }
+        return "OK";
+    }
+
+    private JSONObject buildMessage(JSONObject email) {
+        String messageBody = convertToMarkdown(email);
+        String messageFrom = StringEscapeUtils.escapeHtml4(getFrom(email));
+        String fromEmail = getFromEmail(email);
+        String subject = getSubject(email);
+
+        String messageText = String.format("*Subject:* %s\n*From: *<mailto:%s|%s>\n\n%s",
+                subject,
+                fromEmail,
+                messageFrom,
+                messageBody);
+
+        JSONObject build = new JSONObject();
+        build.put("text", messageText);
+
+        return build;
+    }
+
+    private String getFromEmail(JSONObject email) {
+        JSONObject envelope = (JSONObject) email.get("envelope");
+        return envelope == null ? "" : (String) envelope.get("from");
+    }
+
+    private String getSubject(JSONObject email) {
+        JSONObject headers = (JSONObject) email.get("headers");
+        return headers == null ? "No Subject" : (String) headers.get("subject");
+    }
+
+    private static String getFrom(JSONObject email) {
+        JSONObject headers = (JSONObject) email.get("headers");
+        return headers == null ? "Unknown" : (String) headers.get("from");
+    }
+
+    private String convertToMarkdown(JSONObject email) {
+        String bodyHtml = (String)email.get("html");
+        Remark remark = new Remark();
+        return remark.convert(bodyHtml);
     }
 }
